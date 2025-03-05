2abc1d66364b08f9e47c5ba0a3283d9bad19e90b
RobertASmith <rsmith@darkpeakanalytics.com>
2025-03-05 10:03:04 +0000

    adding the report

--- analysis.R
+++ analysis.R
@@ -3,7 +3,20 @@
 # The report & analysis is trivially simple, but the report PDF should update
 # every time there is a change to the data.
 #
-time <-  Sys.time()
 
-write.csv(x = time,
-          file = "time.csv")
+# Read in the data & calculate the ICER
+df_inputs <- read.csv(file = "data/df_inputs.csv", 
+                      row.names = 1)
+
+# Calculate the incremental costs and qalys
+incCost <- df_inputs["cost", "trt"] - df_inputs["cost", "soc"]
+incQaly <- df_inputs["qaly", "trt"] - df_inputs["qaly", "soc"]
+
+# Calculate the ICER
+ICER <- incCost / incQaly
+
+# render the Rmarkdown report from the script.
+rmarkdown::render(input = "report/report.Rmd",
+                  params = list(ICER = ICER),
+                  output_dir = "outputs",
+                  output_file = "report.pdf")
