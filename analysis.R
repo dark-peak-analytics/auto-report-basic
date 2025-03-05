3f109923f81220ba6d31f447a8faca1d95490549
RobertASmith <rsmith@darkpeakanalytics.com>
2025-03-05 11:15:06 +0000

    minor updates

--- analysis.R
+++ analysis.R
@@ -1,14 +1,15 @@
 # This example is designed to show how a report could be updated using GitHub Actions. 
-# 
-# The report & analysis is trivially simple, but the report PDF should update
-# every time there is a change to the data.
-#
+
+# The script reads a csv file, calculates an ICER from those values, and then passes
+# the ICER to a RMarkdown report.
+
+# This script will be run from a GitHub Actions workflow.
 
 # Read in the data & calculate the ICER
 df_inputs <- read.csv(file = "data/df_inputs.csv", 
                       row.names = 1)
 
-# Calculate the incremental costs and qalys
+# Calculate the incremental costs and qalys for trt vs soc
 incCost <- df_inputs["cost", "trt"] - df_inputs["cost", "soc"]
 incQaly <- df_inputs["qaly", "trt"] - df_inputs["qaly", "soc"]
 
