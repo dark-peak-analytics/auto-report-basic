20ab566ca540bb4912c80b22324ce883f7866f98
RobertASmith <rsmith@darkpeakanalytics.com>
2025-03-05 09:25:32 +0000

    automatically merging

--- analysis.R
+++ analysis.R
@@ -1,5 +1,8 @@
-# Example analysis file
-
+# This example is designed to show how a report could be updated using GitHub Actions. 
+# 
+# The report & analysis is trivially simple, but the report PDF should update
+# every time there is a change to the data.
+#
 time <-  Sys.time()
 
 write.csv(x = time,
